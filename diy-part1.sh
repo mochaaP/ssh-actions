6da727f1abb2ee1ebf5b51fba1bc6c1175ad7155
Jayantajit Gogoi <30702133+jayanta525@users.noreply.github.com>
2020-06-26 08:03:25 +0530

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,7 +2,9 @@
 #=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Description: OpenWrt DIY scri
+
+#pt part 1 (Before Update feeds)
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
